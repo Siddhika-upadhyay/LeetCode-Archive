5a0052edc27f419608e0cc5b41445d99c64f781d
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-08-15 13:59:48 +0530

    Time: 7 ms (53.52%), Space: 42.7 MB (10.46%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -1,23 +1,42 @@
 class Solution {
-    private int f(int i,int []arr,int k,int []dp){
+    // private int f(int i,int []arr,int k,int []dp){
+    //     int n= arr.length;
+    //     if (i==n) return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     int len=0;
+    //     int maxi= Integer.MIN_VALUE;
+    //     int maxAns= Integer.MIN_VALUE;
+    //     for(int j=i;j<Math.min(i+k,n);j++){
+    //         len++;
+    //         maxi= Math.max(maxi,arr[j]);
+    //         int sum= len*maxi+f(j+1,arr,k,dp);
+    //         maxAns=Math.max(maxAns,sum);
+    //     }
+    //     return  dp[i]=maxAns;
+    // }
+    // public int maxSumAfterPartitioning(int[] arr, int k) {
+    //     int n= arr.length;
+    //     int [] dp= new int[n];
+    //     Arrays.fill(dp,-1);
+    //     return f(0,arr,k,dp);
+    // }
+
+    //tabulation:
+    public int maxSumAfterPartitioning(int[] arr, int k) {
         int n= arr.length;
-        if (i==n) return 0;
-        if(dp[i]!=-1)return dp[i];
-        int len=0;
+        int [] dp= new int[n+1];
+        for(int i =n-1;i>=0;i--){
+                int len=0;
         int maxi= Integer.MIN_VALUE;
         int maxAns= Integer.MIN_VALUE;
         for(int j=i;j<Math.min(i+k,n);j++){
-            len++;
-            maxi= Math.max(maxi,arr[j]);
-            int sum= len*maxi+f(j+1,arr,k,dp);
-            maxAns=Math.max(maxAns,sum);
+                len++;
+                maxi= Math.max(maxi,arr[j]);
+                int sum= len*maxi+dp[j+1];
+                maxAns=Math.max(maxAns,sum);
         }
-        return  dp[i]=maxAns;
-    }
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        int n= arr.length;
-        int [] dp= new int[n];
-        Arrays.fill(dp,-1);
-        return f(0,arr,k,dp);
+          dp[i]=maxAns;
+        }
+       return   dp[0];
     }
 }
