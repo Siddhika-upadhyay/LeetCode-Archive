0fc0413a905b30b231b2bd1d54163b904970de72
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-09-09 17:02:31 +0530

    Time: 2 ms (60.26%), Space: 44.2 MB (77.77%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,30 +14,24 @@
  * }
  */
 class Solution {
-   private Map<Integer, Integer> inorderIndexMap;
-
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        inorderIndexMap = new HashMap<>();
-        for(int i = 0; i < inorder.length; i++) {
-            inorderIndexMap.put(inorder[i], i);
-        }
-        
-        return build(preorder, 0, preorder.length - 1, 0, inorder.length - 1);
-    }
-    
-    private TreeNode build(int[] preorder, int preStart, int preEnd, int inStart, int inEnd) {
-        if(preStart > preEnd || inStart > inEnd) {
-            return null;
-        }
-        int rootVal = preorder[preStart];
-        TreeNode root = new TreeNode(rootVal);
-        int inRootIndex = inorderIndexMap.get(rootVal);
-        int leftTreeSize = inRootIndex - inStart;
-        root.left = build(preorder, preStart + 1, preStart + leftTreeSize,
-                          inStart, inRootIndex - 1);
-        root.right = build(preorder, preStart + leftTreeSize + 1, preEnd,
-                           inRootIndex + 1, inEnd);
-        
+    public TreeNode build (int []preorder,int preStart,int preEnd,int []inorder , int inStart,int inEnd,Map<Integer,Integer>inorderMap){
+        if(preStart>preEnd||inStart>inEnd)return null;
+        TreeNode root=new TreeNode(preorder[preStart]);
+        int inRoot= inorderMap.get(root.val);
+        int numsLeft= inRoot-inStart;
+         
+        root.left= build(preorder,preStart+1,preStart+numsLeft,inorder,inStart,
+        inRoot-1,inorderMap);
+        root.right= build(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,inorderMap);
         return root;
-}
+
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer,Integer> inorderMap= new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            inorderMap.put(inorder[i],i);
+        }
+        return build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,  
+        inorderMap);
+    }
 }
