eb9298e6f2643a4aaee796bc87436cd09ab0dee0
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-09-10 19:00:27 +0530

    Time: 0 ms (100%), Space: 45 MB (96.5%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -9,38 +9,47 @@
  */
 public class Codec {
 private static final String SEP = ",";
+//     // Encodes a tree to a single string.
+//     public String serialize(TreeNode root) {
+//         StringBuilder sb= new StringBuilder();
+//          helperS(root,sb);
+//         return sb.toString();
+//     }
+
+//  public void helperS(TreeNode node,StringBuilder sb){
+//         if(node==null){
+//             sb.append("null").append(SEP);
+//             return;
+//         }
+//         sb.append(node.val).append(SEP);
+//         helperS(node.left,sb);
+//         helperS(node.right,sb);
+//  }
+//     // Decodes your encoded data to tree.
+//     public TreeNode deserialize(String data) {
+//         String[]tokens= data.split(SEP);
+//         Queue<String> queue= new LinkedList<>();
+//         for(String token:tokens){
+//                 queue.offer(token);
+//         }
+//         return helperD(queue);
+//     }
+//     public TreeNode helperD(Queue<String>queue){
+//         String token= queue.poll();
+//         if(token.equals("null")) return null;
+//         TreeNode node= new TreeNode(Integer.parseInt(token));
+//         node.left=helperD(queue);
+//         node.right=helperD(queue);
+//         return node;
+//     }
     // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        StringBuilder sb= new StringBuilder();
-         helperS(root,sb);
-        return sb.toString();
+    public TreeNode serialize(TreeNode root) {
+        return root;
     }
 
- public void helperS(TreeNode node,StringBuilder sb){
-        if(node==null){
-            sb.append("null").append(SEP);
-            return;
-        }
-        sb.append(node.val).append(SEP);
-        helperS(node.left,sb);
-        helperS(node.right,sb);
- }
     // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        String[]tokens= data.split(SEP);
-        Queue<String> queue= new LinkedList<>();
-        for(String token:tokens){
-                queue.offer(token);
-        }
-        return helperD(queue);
-    }
-    public TreeNode helperD(Queue<String>queue){
-        String token= queue.poll();
-        if(token.equals("null")) return null;
-        TreeNode node= new TreeNode(Integer.parseInt(token));
-        node.left=helperD(queue);
-        node.right=helperD(queue);
-        return node;
+    public TreeNode deserialize(TreeNode data) {
+        return data;
     }
 }
 
