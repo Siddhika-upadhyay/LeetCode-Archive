34ad748d3cf51f918a507a28cc955267b9ee85df
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-11-10 09:38:11 +0530

    Time: 2 ms (99.76%), Space: 44 MB (9.19%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -14,28 +14,27 @@
  * }
  */
 class Solution {
-    private void preorder(TreeNode node, int vertical, int level,
-                          TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> nodes) {
-        if (node == null) return;
-        if (!nodes.containsKey(vertical)) {
-            nodes.put(vertical, new TreeMap<>());
+    public void preorder(TreeNode node,int vertical,int level,  TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>>nodes){
+         if(node==null)return;
+        if(!nodes.containsKey(vertical)){
+            nodes.put(vertical,new TreeMap<>());
         }
-        if (!nodes.get(vertical).containsKey(level)) {
-            nodes.get(vertical).put(level, new PriorityQueue<>());
+        if(!nodes.get(vertical).containsKey(level)){
+            nodes.get(vertical).put(level,new PriorityQueue<>());
         }
         nodes.get(vertical).get(level).add(node.val);
-        preorder(node.left, vertical - 1, level + 1, nodes);
-        preorder(node.right, vertical + 1, level + 1, nodes);
+        preorder(node.left,vertical-1,level+1,nodes);
+        preorder(node.right,vertical+1,level+1,nodes);
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> nodes = new TreeMap<>();
-        preorder(root, 0, 0, nodes);
-        List<List<Integer>> ans = new ArrayList<>();
-        for (TreeMap<Integer, PriorityQueue<Integer>> levelMap : nodes.values()) {
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>>nodes= new TreeMap<>();
+        preorder(root,0,0,nodes);
+       List<List<Integer>>ans= new ArrayList<>();
+       for (TreeMap<Integer, PriorityQueue<Integer>> levels : nodes.values()) {
             List<Integer> col = new ArrayList<>();
-            for (PriorityQueue<Integer> pq : levelMap.values()) {
+            for (PriorityQueue<Integer> pq : levels.values()) {
                 while (!pq.isEmpty()) {
-                    col.add(pq.poll());
+                    col.add(pq.poll()); 
                 }
             }
             ans.add(col);
