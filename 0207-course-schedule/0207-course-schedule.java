c8ce615bc82395f2e8c562ed9ad00ad8275d0a0a
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-11-12 18:59:32 +0530

    Time: 8 ms (28.97%), Space: 47.1 MB (11.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,65 +1,36 @@
 class Solution {
-    public boolean dfs(int node, int[] vis, int[] dfsVis, List<List<Integer>> adj) {
-        vis[node] = 1;
-        dfsVis[node] = 1;
-        for (int it : adj.get(node)) {
-            if (vis[it] == 0) {
-                if (dfs(it, vis, dfsVis, adj)) return true;
-            } else if (dfsVis[it] == 1) {
-                return true;
-            }
-        }
-        dfsVis[node] = 0;
-        return false;
-    }
-
-    public boolean canFinish(int V, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int V= numCourses;
+        List<List<Integer>>adj= new ArrayList<>();
         for (int i = 0; i < V; i++) {
             adj.add(new ArrayList<>());
-        }
+       }
 
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj.get(u).add(v); 
+        for(int[]edges: prerequisites){
+            int u= edges[0];
+            int v=edges[1];
+            adj.get(v).add(u);
         }
-
-        int[] vis = new int[V];
-        int[] dfsVis = new int[V];
-        for (int i = 0; i < V; i++) {
-            if (vis[i] == 0) {
-                if (dfs(i, vis, dfsVis, adj)) {
-                    return false; 
-                }
+        int[]indegree= new int[V];
+        for(int i=0;i<V;i++){
+            for(int it:adj.get(i)){
+                    indegree[it]++;
             }
         }
-        return true; 
-            //   int indegree[]=new int[V];
-            //   for(int i=0;i<V;i++){
-            //       for(int it:adj.get(i)){
-            //              indegree[it]++;
-            //       }
-            //   }
-            //   Queue<Integer>q= new LinkedList<Integer>();
-            //   for(int i=0;i<V;i++){
-            //       if(indegree[i]==0){
-            //           q.add(i);
-            //       }
-            //   }
-            //   ArrayList<Integer>topo= new ArrayList<>();
-            //   while(!q.isEmpty()){
-            //       int node = q.peek();
-            //       q.remove();
-            //       topo.add(node);
-            //       for(int it:adj.get(node)){
-            //           indegree[it]--;
-            //           if(indegree[it]==0){
-            //               q.add(it);
-            //           }
-            //       }
-            //   }
-            //   if(topo.size()==V)return true;
-            //   return false;
+        Queue<Integer>q= new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)q.add(i);
+        }
+        ArrayList<Integer>topo= new ArrayList<>();
+        while(!q.isEmpty()){
+           int node = q.poll();
+           topo.add(node);
+           for(int it:adj.get(node)){
+            indegree[it]--;
+            if(indegree[it]==0)q.add(it);
+           }
+        }
+        if(topo.size()!=V)return false;
+        return true;
     }
 }
