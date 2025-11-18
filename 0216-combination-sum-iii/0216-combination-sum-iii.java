db25f64c52218d2fe27dd7b7b041b64227cd13dc
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-11-18 11:12:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (24.02%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -4,12 +4,32 @@ class Solution {
      comb(1,k,n,new ArrayList<>(),ans);
      return ans;   
     }
-    public void comb(int ind, int k , int n , ArrayList<Integer>arr, List<List<Integer>> ans ){
+    // public void comb(int ind, int k , int n , ArrayList<Integer>arr, List<List<Integer>> ans ){
+    //     if(k==0&&n==0){
+    //         ans.add(new ArrayList<>(arr));
+    //         return;
+    //     }
+    //     if(k==0||n<0)return;
+    //     for(int i=ind;i<=9;i++){
+    //         if (i > n) break;
+    //         arr.add(i);
+    //         comb(i+1,k-1,n-i,arr,ans);
+    //         arr.remove(arr.size()-1);
+    //     }
+    // }
+
+    //with pruning :
+    public void comb(int ind, int k , int n , ArrayList<Integer>arr, 
+    List<List<Integer>> ans ){
         if(k==0&&n==0){
             ans.add(new ArrayList<>(arr));
             return;
         }
         if(k==0||n<0)return;
+        int minSum= (2*ind+k-1)*k/2;
+        if(n<minSum)return;
+        int maxSum=(2*9-k+1)*k/2;
+        if(n>maxSum)return;
         for(int i=ind;i<=9;i++){
             if (i > n) break;
             arr.add(i);
