df1279652801b6e2ae4bb5b8904eec6236a0a70b
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-11-20 12:24:18 +0530

    Time: 1 ms (99.76%), Space: 46.6 MB (17.11%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,43 +1,99 @@
 class Solution {
-    static void solve(int col,char[][]board,List<List<String>> res,int []leftRow,int[]lowerDiagonal,int[]upperDiagonal){
-        if(col==board.length){
-           res.add(construct(board));
-           return ;
+    // public boolean isSafe(int i, int j , char [][]board, int n){
+    //     //upper diagonal
+    //     int row=i;
+    //     int col=j;
+    //     while(i>=0&&j>=0){
+    //         if(board[i][j]=='Q')return false;
+    //         i--;
+    //         j--;
+    //     }
+    //     i=row;j=col;
+    //     //lower diagonal
+    //     while(i<n&&j>=0){
+    //         if(board[i][j]=='Q')return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     i=row;j=col;
+    //     while(j>=0){
+    //         if(board[i][j]=='Q')return false;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+
+    // public void solve(int col, char [][]board ,List<List<String>>res, int n){
+    //     if(col==n){
+    //         List<String>temp= new ArrayList<>();
+    //         for(int i=0;i<n;i++){
+    //             temp.add(new String(board[i]));
+    //         }
+    //         res.add(temp);
+    //         return;
+    //     }
+
+    //     for(int row=0;row<n;row++){
+    //         if(isSafe(row,col,board,n)){
+    //             board[row][col]='Q';
+    //             solve(col+1,board,res,n);
+    //             board[row][col]='.';
+    //         }
+    //     }
+    // }
+    // public List<List<String>> solveNQueens(int n) {
+    //     char board[][] = new char [n][n];
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             board[i][j] = '.';
+    //         }
+    //     }
+    //     List<List<String>> res = new ArrayList<>();
+    //     solve(0, board, res, n);
+    //     return res;
+    // }
+
+
+//optimized answer for this ::
+
+
+public void solve(int col, char [][]board ,List<List<String>>res, int n,
+    int []leftRow,int []lowerDiagonal,int []upperDiagonal){
+        if(col==n){
+            List<String>temp= new ArrayList<>();
+            for(int i=0;i<n;i++){
+                temp.add(new String(board[i]));
+            }
+            res.add(temp);
+            return;
         }
-        for(int row=0;row<board.length;row++){
-            if(leftRow[row]==0&&lowerDiagonal[row+col]==0&&upperDiagonal[board.length-1+col-row]==0){
+
+        for(int row=0;row<n;row++){
+            if(leftRow[row]==0&&lowerDiagonal[row+col]==0 && upperDiagonal[(n-1)+(col-row)]==0){
                 board[row][col]='Q';
                 leftRow[row]=1;
                 lowerDiagonal[row+col]=1;
-                upperDiagonal[board.length-1+col-row]=1;
-                solve(col+1,board,res,leftRow,lowerDiagonal,upperDiagonal);
+                upperDiagonal[(n-1)+(col-row)]=1;
+                solve(col+1,board,res,n,leftRow,lowerDiagonal,upperDiagonal);
                 board[row][col]='.';
                 leftRow[row]=0;
                 lowerDiagonal[row+col]=0;
-                upperDiagonal[board.length-1+col-row]=0;
+                upperDiagonal[(n-1)+(col-row)]=0;
             }
         }
     }
     public List<List<String>> solveNQueens(int n) {
-       char[][]board =new char[n][n];
-       for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            board[i][j]='.';
+        char board[][] = new char [n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
         }
-       } 
-       List<List<String>>res=new ArrayList<List<String>>();
+        List<List<String>> res = new ArrayList<>();
        int leftRow[]=new int[n];
        int upperDiagonal[]=new int[2*n-1];
        int lowerDiagonal[]=new int[2*n-1];
-       solve(0,board,res,leftRow,lowerDiagonal,upperDiagonal);
-       return res;
-    }
-     static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
-        }
+       solve(0,board,res,n,leftRow,lowerDiagonal,upperDiagonal);
         return res;
     }
 }
