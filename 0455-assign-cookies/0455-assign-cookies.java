54a97d03968c6eb511e9622e8569f374bed719d1
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-12-03 11:21:30 +0530

    Time: 15 ms (34.07%), Space: 51.2 MB (43.65%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
         int i=0,j=0;
-        while(i<g.length&&j<s.length){
+        while(i<g.length && j<s.length){
             if(s[j]>=g[i]){
                 i++;
             }
