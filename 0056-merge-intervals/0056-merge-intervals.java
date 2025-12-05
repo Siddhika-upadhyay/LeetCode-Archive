bc9264db07f4d8cb7d753da22e111828cc1cbf37
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-12-05 14:40:44 +0530

    Time: 11 ms (14.02%), Space: 49 MB (27.53%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,16 @@
-import java.util.*;
 class Solution {
-    public int[][] merge(int[][] arr) {
-     int n = arr.length;
-        Arrays.sort(arr, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
+    public int[][] merge(int[][] intervals) {
+        int n= intervals.length;
+        ArrayList<int[]>ans= new ArrayList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        for( int i=0;i<n;i++){
+            if (ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]) {
+                ans.add(new int[]{intervals[i][0], intervals[i][1]});
             }
-        });
-
-        List<int[]> ans = new ArrayList<>(); 
-        for (int i = 0; i < n; i++) {
-            if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1)[1]) {
-                ans.add(new int[]{arr[i][0], arr[i][1]});
-            }
-            
             else {
-                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], arr[i][1]);
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
             }
         }
-
-        return ans.toArray(new int[ans.size()][]);
+       return ans.toArray(new int[ans.size()][]);
     }
 }
