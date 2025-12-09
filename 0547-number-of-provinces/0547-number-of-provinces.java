584925427a6fa2939c6a891644c51cc3c5846201
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-12-09 16:33:36 +0530

    Time: 1 ms (86.28%), Space: 47 MB (84.59%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,72 +1,23 @@
-class DisjointSet{
-    List<Integer> parent=new ArrayList<>();
-    List<Integer> size=new ArrayList<>();
-    public DisjointSet(int n){
-        for(int i=0;i<=n;i++){
-            size.add(1);
-            parent.add(i);
-        }
-    }
-    public int findUPar(int node){
-        if(node==parent.get(node))return node;
-        int ulp=findUPar(parent.get(node));
-        parent.set(node,ulp);
-        return parent.get(node);
-    }
-    public void unionBySize(int u,int v){
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-        if(ulp_v==ulp_u)return;
-        if(size.get(ulp_u)<size.get(ulp_v)){
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
-        }
-        else{
-            parent.set(ulp_v,ulp_u);
-            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
-        }
-    }
-}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-       int n = isConnected.length;
-       DisjointSet ds=new DisjointSet(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    ds.unionBySize(i, j);
-                }
+        int n= isConnected.length;
+        int m= isConnected[0].length;
+        int vis[]= new int [n];
+        int count=0;
+        for(int i=0;i<n;i++){
+                if(vis[i]==0){
+                    count++;
+                    dfs(i, vis, isConnected);  
             }
-        }
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (ds.findUPar(i) == i) count++;
         }
         return count;
     }
+    public void dfs( int i, int []vis, int [][]isConnected){
+        vis[i]=1;
+           for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j]==1 &&vis[j]==0){
+                dfs(j,vis,isConnected);
+            }
+        }
+    }
 }
-
-//using dfs
-
-// class Solution {
-//     public int findCircleNum(int[][] isConnected) {
-//         int n= isConnected.length;
-//         boolean vis[]= new boolean[n];
-//         int count=0;
-//         for(int i=0;i<n;i++){
-//             if(!vis[i]){
-//                 count++;
-//                 dfs(i,isConnected,vis);
-//             }
-//         }
-//         return count;
-//     }
-//     public static void dfs(int i,int [][]isConnected,boolean[]vis){
-//         vis[i]=true;
-//         for(int j=0;j<isConnected.length;j++){
-//             if(isConnected[i][j]==1 &&!vis[j]){
-//                 dfs(j,isConnected,vis);
-//             }
-//         }
-//     }
-// }
