c6ebcf6eab6cfbe52d77c3871d04967a946655cf
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-12-10 12:14:29 +0530

    Time: 1 ms (44.27%), Space: 46.7 MB (49.03%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-         int iniColor=image[sr][sc];
-         int[][]ans= image;
-         int delRow[]={-1,0,1,0};
-         int delCol[]={0,1,0,-1};
-         dfs(sr,sc,ans,image,color,delRow,delCol,iniColor);
-         return ans;
-    }
-    public static void dfs(int row,int col,int[][]ans,int[][]image,int newColor,int delRow[],int delCol[],int iniColor){
-        ans[row][col]=newColor;
-        int n= image.length;
-        int m= image[0].length;
+       int n= image.length;
+       int m= image[0].length;
+       Queue<int[]> q= new LinkedList<>();
+       int []dr={-1,0,1,0};
+       int []dc={0,1,0,-1};
+       int originalColor= image[sr][sc];
+       if(originalColor== color)return image;
+      q.add(new int[]{sr,sc});
+      while(!q.isEmpty()){
+        int []arr= q.poll();
+        int r= arr[0];
+        int c= arr[1];
+        image[r][c]=color;
         for(int i=0;i<4;i++){
-            int nrow=row+delRow[i];
-            int ncol=col+delCol[i];
-        if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&image[nrow][ncol]==iniColor&&ans[nrow][ncol]!=newColor){
-            dfs(nrow,ncol,ans,image,newColor,delRow,delCol,iniColor);
+            int nr= r+dr[i];
+            int nc= c+dc[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==originalColor){
+                q.add(new int[]{nr,nc});
+            }
         }
-        }
+      }
+      return image;
     }
 }
