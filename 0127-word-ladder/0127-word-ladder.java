ef90259cb0fae0fba08f322b7c40ff786fa111d2
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-12-11 12:30:16 +0530

    Time: 68 ms (73.3%), Space: 48 MB (53.04%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,31 +1,36 @@
 class Solution {
+    class Pair{
+        String word;
+        int steps;
+        Pair(String w, int s){
+            this.word=w;
+            this.steps=s;
+        }
+    }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String>set= new HashSet<>(wordList);
-        if(!set.contains(endWord))return 0;
-        Queue<String>q= new LinkedList<>();
-        q.add(beginWord);
-        int level=1;
+        int n= beginWord.length();
+        Set<String> wordSet= new HashSet<>(wordList);
+        if(!wordSet.contains(endWord))return 0;
+        Queue<Pair>q= new LinkedList<>();
+        q.add(new Pair(beginWord,1));
         while(!q.isEmpty()){
-            int size= q.size();
-            for(int i=0;i<size;i++){
-                String word= q.poll();
-            char[] wordArr= word.toCharArray();
-            for(int j=0;j< wordArr.length;j++){
-              char originalWord=  wordArr[j];
-              for(char c='a'; c<='z';c++){
-                    wordArr[j]=c;
-                  //change this changed wordArr to string:
-                  String temp= new String(wordArr);
-                  if(temp.equals(endWord))return level+1;
-                  if(set.contains(temp)){
-                    q.add(temp);
-                    set.remove(temp);
-                  }
-              }
-              wordArr[j]=originalWord;
+            Pair temp= q.poll();
+            String word= temp.word;
+            int steps= temp.steps;
+            if(word.equals(endWord))return steps;
+            char[]wordArray= word.toCharArray();
+            for(int i=0;i<wordArray.length;i++){
+                char originalChar= wordArray[i];
+                for(char c='a';c<='z';c++){
+                    wordArray[i]=c;
+                    String tempString=new String(wordArray);
+                    if(wordSet.contains(tempString)){
+                        q.add(new Pair(tempString,steps+1));
+                         wordSet.remove(tempString);
+                    }
+                }
+                wordArray[i]=originalChar;
             }
-            }
-            level++;
         }
         return 0;
     }
