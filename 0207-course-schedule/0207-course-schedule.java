68fcd176cd6a62d7dcd12401d06efae63760bb1c
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2025-12-26 11:13:14 +0530

    Time: 6 ms (75.01%), Space: 47.2 MB (28.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>>adj= new ArrayList<>();
         int V= numCourses;
-        List<List<Integer>>adj= new ArrayList<>();
-        for (int i = 0; i < V; i++) {
+        for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
-       }
-
-        for(int[]edges: prerequisites){
-            int u= edges[0];
-            int v=edges[1];
-            adj.get(v).add(u);
         }
-        int[]indegree= new int[V];
+        for( int i=0;i< prerequisites.length;i++){
+            int u= prerequisites[i][0];
+            int v=  prerequisites[i][1];
+            adj.get(u).add(v);
+        }
+        int []indegree=new int[V];
         for(int i=0;i<V;i++){
             for(int it:adj.get(i)){
-                    indegree[it]++;
+                indegree[it]++;
             }
         }
         Queue<Integer>q= new LinkedList<>();
         for(int i=0;i<V;i++){
-            if(indegree[i]==0)q.add(i);
+            if(indegree[i]==0){
+                q.add(i);
+            }
         }
-        ArrayList<Integer>topo= new ArrayList<>();
+        ArrayList<Integer> arr= new ArrayList<>();
         while(!q.isEmpty()){
-           int node = q.poll();
-           topo.add(node);
-           for(int it:adj.get(node)){
-            indegree[it]--;
-            if(indegree[it]==0)q.add(it);
-           }
+            int num= q.poll();
+            arr.add(num);
+            for(int it: adj.get(num)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                  q.add(it);
+                }
+            }
         }
-        if(topo.size()!=V)return false;
+        if(arr.size()!=V)return false;
         return true;
     }
 }
