d71cf7339b959d5abb2c3c49dfcc9551c2d527ea
siddhika upadhyay <154582510+Siddhika-upadhyay@users.noreply.github.com>
2026-01-07 18:50:55 +0530

    Time: 7 ms (38.84%), Space: 48.7 MB (6.1%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,41 +1,44 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>>adj= new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        ArrayList<ArrayList<Integer>>adj= new ArrayList<>();
+        int V= numCourses;
+        for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<prerequisites.length;i++){
+        for( int i=0;i< prerequisites.length;i++){
             int u= prerequisites[i][0];
-            int v= prerequisites[i][1];
+            int v=  prerequisites[i][1];
             adj.get(v).add(u);
         }
-        int indegree[]= new int[numCourses];
-        for(int i=0;i<numCourses;i++){
+        int []indegree=new int[V];
+        for(int i=0;i<V;i++){
             for(int it:adj.get(i)){
                 indegree[it]++;
             }
         }
         Queue<Integer>q= new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0)q.add(i);
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
         }
-        int topo[]=new int[numCourses];
-        int i=0;
+        ArrayList<Integer> arr= new ArrayList<>();
         while(!q.isEmpty()){
-            int node= q.peek();
-            q.remove();
-            topo[i++]=node;
-            for(int it:adj.get(node)){
+            int num= q.poll();
+            arr.add(num);
+            for(int it: adj.get(num)){
                 indegree[it]--;
                 if(indegree[it]==0){
-                    q.add(it);
+                  q.add(it);
                 }
             }
         }
-      if (i == numCourses)
-     return topo;
-     else
-     return new int[0];
+        if(arr.size() != V) return new int[0];
 
+        int[] result = new int[V];
+        for(int i = 0; i < V; i++) {
+            result[i] = arr.get(i);
+        }
+        return result;
     }
 }
